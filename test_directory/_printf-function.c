857ea67addcddce1e60e347a500fb484bc2f0862
Lhaniii <ilhan95210@icloud.com>
2024-03-26 15:11:54 +0000

    update of prototype

--- test_directory/_printf-function.c
+++ test_directory/_printf-function.c
@@ -4,42 +4,59 @@
  * Description: a function that prints a string.
  *
  */
-void print_string(va_list list)
+int print_string(va_list list)
 {
+	
         int j;
         char *string = va_arg(list, char *);
         if (string == NULL)
         {
                 _putchar('n');
-                return;
         }
         for (j = 0; string[j] != '\0'; j++)
         {
                 _putchar(string[j]);
         }
+	return (0);
+	 
+
+	/**
+	char *str;
+	int len, i;
+
+	str = va_arg(list, char *);
+	len = _strlen(str);
+
+	for (i = 0; i < len; i++)
+	{
+		_putchar (str[i]);
+	}
+	return (0);
+	*/
 }
 /**
  * print_char - a function that prints a character.
  * Description: a function that prints a character.
  *
  */
-void print_char(va_list list)
+int print_char(va_list list)
 {
         char c = va_arg(list, int);
-        _putchar(c);
+        return(c);
+	
 }
 /**
  * print_percentage - a function that prints the percentage sign.
  * Description:
  *
  */
-void print_percentage(va_list list)
+int print_percentage(va_list list)
 {
 	char *str = "%";
-	
+
 	if (va_arg(list, int) == *str)
 	{
-		_putchar(*str);
+		return (*str);
 	}
-        _putchar(*str);
+	return (*str);
 }
--- test_directory/main.h
+++ test_directory/main.h
@@ -6,14 +6,14 @@
 typedef struct spe
 {
         char spe;
-        void (*f)(va_list list);
+        int (*f)(va_list list);
 } spe_t;
 int _putchar(char c);
 int _printf(const char *format, ...);
 int _strlen(char *s);
-void print_char(va_list list);
-void print_string(va_list list);
-void print_percentage(va_list list);
-void print_decimal(va_list list);
-void print_integer(va_list list);
+int print_char(va_list list);
+int print_string(va_list list);
+int print_percentage(va_list list);
+int print_decimal(va_list list);
+int print_integer(va_list list);
 #endif

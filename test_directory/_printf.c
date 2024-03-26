034b040eb7d2384970d653b894863153f547444f
omega-jean <jeanalex1405@gmail.com>
2024-03-26 14:13:42 +0000

    Add the code of the function

--- test_directory/_printf.c
+++ test_directory/_printf.c
@@ -1,25 +1,36 @@
 #include "main.h"
-
 /**
- * _printf - print all 
+ * _printf - print all
  *
  * @format: is a character string.
  *
- * return: ..
+ * return: always 0.
  */
-
 int _printf(const char *format, ...)
 {
-va_list list;
-
-spe_t sps[] = {
-	{"c", print_char},
-	{"s", print_string},
-	{"%", print_percentage},
-	{"d", print_decimal},
-	{"i", print_integer},
-	{'\0', NULL}
-};
-
-print_string("Hello, test")
+        va_list list;
+        int i, j;
+        spe_t sps[] = {
+                {'c', print_char},
+                {'s', print_string},
+                {'%', print_percentage},
+                {'\0', NULL}};
+        va_start(list, format);
+        j = 0;
+        while (format != NULL && format[j] != '\0')
+        {
+                i = 0;
+                while (sps[i].spe != '\0')
+                {
+                        if (sps[i].spe == format[j])
+                        {
+                                sps[i].f(list);
+                        }
+                        i++;
+                }
+                j++;
+        }
+        va_end(list);
+        _putchar('\n');
+        return (0);
 }

6efd176d3e5913a17b5bab5a0670f38e23de3fa8
omega-jean <jeanalex1405@gmail.com>
2024-03-26 14:07:30 +0000

    fix code for the function print_percentage

--- test_directory/_printf-function.c
+++ test_directory/_printf-function.c
@@ -1,98 +1,39 @@
 #include "main.h"
-
-/**
- * print_char - a function that prints a character.
- * Description: a function that prints a character.
- *
- */
-
-void print_char(va_list list)
-{
-	char c;
-
-	c = va_arg(list, int);
-	_putchar(c);
-	_putchar('\n');
-}
-
 /**
  * print_string - a function that prints a string.
  * Description: a function that prints a string.
  *
  */
-
 void print_string(va_list list)
 {
-	int i, j, length_string = 0;
-	char *string = va_arg(list, char *);
-
-	if (string == NULL)
-	{
-		_putchar('n');
-		return;
-	}
-	for (i = 0; i != '\0'; i++)
-	{
-		length_string++;
-	}
-	for (j = 0; j < (length_string - 1); j++)
-	{
-		_putchar(string[j]);
-	}
-        _putchar('\n');
+        int j;
+        char *string = va_arg(list, char *);
+        if (string == NULL)
+        {
+                _putchar('n');
+                return;
+        }
+        for (j = 0; string[j] != '\0'; j++)
+        {
+                _putchar(string[j]);
+        }
 }
-
+/**
+ * print_char - a function that prints a character.
+ * Description: a function that prints a character.
+ *
+ */
+void print_char(va_list list)
+{
+        char c = va_arg(list, int);
+        _putchar(c);
+}
 /**
  * print_percentage - a function that prints the percentage sign.
  * Description:
  *
  */
-
 void print_percentage(va_list list)
 {
-	/**
-	char *str = "%";
-
-	if (va_arg(list, int) == *str)
-	{
-		_putchar(*str);
-	}
-        _putchar('\n');
-	*/
-
-	char percent = '%';
-	int k;
-
-	for (k = 0; k != '\0'; k++)
-	{
-		if (va_arg(list, int) == percent)
-		{
-			_putchar(percent);
-		}
-		_putchar('\n');
-	}
-}
-
-/**
- * print_decimal - a function that prints a decimal (base 10) number.
- * Description:
- *
- */
-
-void print_decimal(va_list list)
-{
-        _putchar(va_arg(list, int));
-        _putchar('\n');
-}
-
-/**
- * print_integer - a function that prints an integer in base 10.
- * Description:
- *
- */
-
-void print_integer(va_list list)
-{
-        _putchar(va_arg(list, int));
-        _putchar('\n');
+        _putchar('%');
 }

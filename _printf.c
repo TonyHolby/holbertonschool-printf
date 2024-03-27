67b124f36dafef12b2cfae7e278a31933ded374f
TonyHolby <tonynemoutheholberton@gmail.com>
2024-03-27 18:36:20 +0000

    Fix betty style description and code

--- _printf.c
+++ _printf.c
@@ -1,27 +1,32 @@
 #include "main.h"
+
 /**
  * _printf - print all
  *
  * @format: is a character string.
  *
- * return: always 0.
+ * Return: the number of characters printed.
  */
 
 int _printf(const char *format, ...)
 {
-        va_list list;
-        int i, j;
-        spe_t sps[] = {
-                {'c', print_char},
-                {'s', print_string},
-                {'%', print_percentage},
-		{'d', print_decimal},
+	va_list list;
+	int i, j, num_of_char = 0;
+	spe_t sps[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'%', print_percentage},
 		{'\0', NULL}};
+
 	va_start(list, format);
 	j = 0;
+	if (format == NULL || format[j + 1] == '\0')
+	{
+		return (-1);
+	}
 	while (format != NULL && format[j] != '\0')
 	{
-		if (format[j] == '%')
+		if (format[j] == '%' && (format[j] != 'K' || format[j] != '!'))
 		{
 			j++;
 			i = 0;
@@ -29,17 +34,18 @@ int _printf(const char *format, ...)
 			{
 				if (sps[i].spe == format[j])
 				{
-					sps[i].f(list);
+					num_of_char += sps[i].f(list);
 				}
 				i++;
 			}
 		}
-		else 
+		else
 		{
 			_putchar(format[j]);
+			num_of_char++;
 		}
 		j++;
 	}
 	va_end(list);
-	return (0);
+	return (num_of_char);
 }

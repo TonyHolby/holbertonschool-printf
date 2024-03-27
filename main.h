997ea67810b490df57a746ab5ff1b3d5c6776565
TonyHolby <tonynemoutheholberton@gmail.com>
2024-03-27 19:00:02 +0000

    Fix betty doc and add a description for the structure

--- main.h
+++ main.h
@@ -1,13 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+
+/**
+ * struct spe - structur that determine the conversion specifiers and
+ * the corresponding function
+ * @spe: is a conversion specifier
+ * @f: is a function pointer that calls the corresponding function
+ */
 typedef struct spe
 {
-        char spe;
-        int (*f)(va_list list);
+	char spe;
+	int (*f)(va_list list);
 } spe_t;
+
 int _putchar(char c);
 int _printf(const char *format, ...);
 int _strlen(char *s);
@@ -16,4 +25,5 @@ int print_string(va_list list);
 int print_percentage(va_list list);
 int print_decimal(va_list list);
 int print_integer(va_list list);
+
 #endif

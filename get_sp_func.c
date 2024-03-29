bca0bb867a87cf5fab3428b2240a9bfeb69e9838
omega-jean <jeanalex1405@gmail.com>
2024-03-29 09:40:37 +0000

    Fix betty-style in get_sp_func.c

--- get_sp_func.c
+++ get_sp_func.c
@@ -11,36 +11,36 @@
 
 int get_sp_func(const char *format, va_list list)
 {
-        int i, j = 0, len = 0;
-        spe_t sps[] = {
-                {'c', print_char},
-                {'s', print_string},
-                {'%', print_percentage},
-                {'d', print_decimal},
-                {'i', print_decimal},
-                {'\0', NULL}};
+	int i, j = 0, len = 0;
+	spe_t sps[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'%', print_percentage},
+		{'d', print_decimal},
+		{'i', print_decimal},
+		{'\0', NULL}};
 
-        while (format != NULL && format[j] != '\0')
-        {
-                if (format[j] == '%' && (format[j + 1] != 'K' && format[j + 1] != '!'))
-                {
-                        j++;
-                        i = 0;
-                        while (sps[i].spe != '\0')
-                        {
-                                if (sps[i].spe == format[j])
-                                {
-                                        len += sps[i].f(list);
-                                }
-                                i++;
-                        }
-                }
-                else
-                {
-                        _putchar(format[j]);
-                        len++;
-                }
-                j++;
-        }
-        return (len);
+	while (format != NULL && format[j] != '\0')
+	{
+		if (format[j] == '%' && (format[j + 1] != 'K' && format[j + 1] != '!'))
+		{
+			j++;
+			i = 0;
+			while (sps[i].spe != '\0')
+			{
+				if (sps[i].spe == format[j])
+				{
+					len += sps[i].f(list);
+				}
+				i++;
+			}
+		}
+		else
+		{
+			_putchar(format[j]);
+			len++;
+		}
+		j++;
+	}
+	return (len);
 }
